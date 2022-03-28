e1d025ab92f1ff5440abe8781e9d1a0153ad5001
EnokiUN <minekillergaming1@gmail.com>
2022-03-28 22:39:16 +0300

    added more stuff

--- bot/main.py
+++ bot/main.py
@@ -1,18 +1,40 @@
 from dotenv import load_dotenv
 import os
 import voltage
+import json
 
-from utils import unwrap, Command, CommandContext, Cog, CommandsClient
+from utils import unwrap, Command, CommandContext, Cog, CommandsClient, CommandNotFound
 
-client = CommandsClient("-")
+async def prefix(messsage):
+    with open ("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+    return prefixes.get(str(messsage.server.id), "-")
+
+client = CommandsClient(prefix)
 
 @client.error('message')
 async def on_message_error(error: Exception, message: voltage.Message):
+    if isinstance(error, CommandNotFound):
+        return
     await message.reply(f"An error has occured: {error}")
-    raise error
 
 client.add_extension("cogs.misc")
 client.add_extension("cogs.fun")
+
+@client.command()
+async def prefix(ctx, prefix):
+    perms = 0
+    for i in ctx.author.roles:
+        perms |= i.permissions.flags
+    perms = voltage.ServerPermissions.new_with_flags(perms)
+    if not perms.kick_members:
+        return await ctx.reply("You don't have permission to change the prefix")
+    with open ("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+    prefixes[str(ctx.server.id)] = prefix
+    with open ("prefixes.json", "w") as f:
+        json.dump(prefixes, f)
+    await ctx.reply(f"Prefix changed to `{prefix}`")
 
 @client.command()
 async def reload(ctx, cog: str):
